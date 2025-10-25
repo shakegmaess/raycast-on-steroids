c548dc64363d7ee48fa96f5a2e970a9c0d285d96
shakegmaess <canaisdoshake@gmail.com>
2025-10-25 17:22:17 -0300

    opengl window

---
+++ scripts/Program.cs
@@ -0,0 +1,41 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Common;
+using System.Numerics;
+using OpenTK.Input;
+using Silk.NET.Core.Native;
+
+class RaycastWindow : GameWindow
+{
+    public RaycastWindow(GameWindowSettings gws, NativeWindowSettings nws) : base(gws, nws)
+    {
+    }
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+        Console.WriteLine("Holy shit a new frame. fps: " + 1 / args.Time);
+
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+}
+
+class Program()
+{
+    static void Main()
+    {
+        var NativeWindowSettings = new NativeWindowSettings()
+        {
+            ClientSize = new Vector2i(800, 600),
+            Title = "Raycast on steroids",
+        };
+
+        using var window = new RaycastWindow(GameWindowSettings.Default, NativeWindowSettings);
+        window.Run();
+    }
+
+}
